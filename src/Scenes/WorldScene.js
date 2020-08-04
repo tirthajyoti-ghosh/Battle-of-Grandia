06df9a021dc076f5b56a36144c2f9fc484f287e4
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-08-05 01:20:57 +0530

    Add battle map
    - Refactor enemy zone dimension

--- src/Scenes/WorldScene.js
+++ src/Scenes/WorldScene.js
@@ -70,10 +70,11 @@ export default class WorldScene extends Phaser.Scene {
     this.spawns = this.physics.add.group({ classType: Phaser.GameObjects.Zone });
 
     for(var i = 0; i < 30; i++) {
-        var x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
-        var y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
-        // parameters are x, y, width, height
-        this.spawns.create(x, y, 20, 20);            
+      var x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+      var y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+
+      // parameters are x, y, width, height
+      this.spawns.create(x, y, 50, 50);            
     }        
 
     this.physics.add.overlap(this.player, this.spawns, this.onMeetEnemy, false, this);

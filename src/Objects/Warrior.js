8b2cc4e7deee7de43db850b46c107cbf40101be5
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-08-06 16:20:41 +0530

    Separate warrior animation

--- src/Objects/Warrior.js
+++ src/Objects/Warrior.js
@@ -4,5 +4,37 @@ import Entity from './Entity';
 export default class Warrior extends Entity {
   constructor(scene) {
     super(scene, 400, 590, 'warrior', 4, 1.5);
+
+    this.scene = scene;
+  }
+
+  createAnimation() {
+    this.scene.anims.create({
+      key: 'left',
+      frames: this.scene.anims.generateFrameNumbers('warrior', { frames: [8, 9, 10, 11]}),
+      frameRate: 10,
+      repeat: -1
+    });
+      
+    this.scene.anims.create({
+      key: 'right',
+      frames: this.scene.anims.generateFrameNumbers('warrior', { frames: [12, 13, 14, 15] }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.scene.anims.create({
+      key: 'up',
+      frames: this.scene.anims.generateFrameNumbers('warrior', { frames: [4, 5, 6, 7]}),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.scene.anims.create({
+      key: 'down',
+      frames: this.scene.anims.generateFrameNumbers('warrior', { frames: [0, 1, 2, 3] }),
+      frameRate: 10,
+      repeat: -1
+    });
   }
 }
--- src/Scenes/BattleScene.js
+++ src/Scenes/BattleScene.js
@@ -38,40 +38,14 @@ export default class BattleScene extends Phaser.Scene {
     
     this.physics.add.collider(this.warrior, border);
     
+    this.warrior.createAnimation();
+
     this.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
       left: Phaser.Input.Keyboard.KeyCodes.A,
       right: Phaser.Input.Keyboard.KeyCodes.D,
-    });
-
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('warrior', { frames: [8, 9, 10, 11]}),
-      frameRate: 10,
-      repeat: -1
-    });
-      
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('warrior', { frames: [12, 13, 14, 15] }),
-      frameRate: 10,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'up',
-      frames: this.anims.generateFrameNumbers('warrior', { frames: [4, 5, 6, 7]}),
-      frameRate: 10,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'down',
-      frames: this.anims.generateFrameNumbers('warrior', { frames: [0, 1, 2, 3] }),
-      frameRate: 10,
-      repeat: -1
-    });
+    });    
     
     this.kraken = new Kraken(this);
 
